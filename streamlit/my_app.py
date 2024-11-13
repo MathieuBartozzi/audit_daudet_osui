b99c7545256aacf45407b20a8907c4e03dc332e5
Mathieu Bartozzi <mathieu.bartozzi@mlfmonde.org>
2024-11-13 14:16:47 +0100

    creation my_app.py

---
+++ streamlit/my_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# Configuration de la mise en page
+st.set_page_config(
+    page_title='Audit LFIAD 2024')
+
+st.sidebar.success("Selectionner une page.")
+
+# Titre de l'application
+st.title("Audit LFIAD 2024 : Analyse des résulats et enquetes")
+
+st.divider()
+
+# Introduction
+st.markdown("""
+Cette interface vous permet de naviguer et d'explorer les données de l'établissement.
+Chaque section présente des graphiques interactifs qui permettent de visualiser efficacement blabla.
+
+""")
+
+st.write('**Bonne navigation !**')
+
+
+
+st.title("titre 1")
+
+st.divider()
